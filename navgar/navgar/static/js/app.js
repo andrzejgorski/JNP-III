25efb680722eef3300f6395db395f518c719d09c
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2015-10-10 15:14:42 +0200

    Deleted unnecessery server url

--- navgar/navgar/static/js/app.js
+++ navgar/navgar/static/js/app.js
@@ -1,12 +1,10 @@
 'use strict';
 
-var serverUrl = 'http://0.0.0.0:6543';
-
 (function(){
     var app = angular.module('navgar', []);
     app.controller('PostsCtrl', ['$http', function($http){
         var self = this;
-        $http.get(serverUrl + '/posts/first').success(function(data){
+        $http.get('/posts/first').success(function(data){
             self.posts = data;
         });
     }]);
