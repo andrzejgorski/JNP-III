0b30de880599e46be73097d40c017430bd30dd8f
Krzysztof Gromadzki <gromadzki.krzysztof@gmail.com>
2015-10-10 12:50:57 +0200

    Attached Angular

--- navgar/navgar/views.py
+++ navgar/navgar/views.py
@@ -1,6 +1,22 @@
 from pyramid.view import view_config
 
+example_posts = [
+    {
+        'author': 'Adam',
+        'content': 'Cześć wszystkim!'
+    },
+    {
+        'author': 'Marcin',
+        'content': 'Co tam?'
+    }
+]
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+
+@view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
-    return {'project': 'navgar'}
+    return {}
+
+
+@view_config(route_name='posts/first', renderer='json')
+def example_post(request):
+    return example_posts
