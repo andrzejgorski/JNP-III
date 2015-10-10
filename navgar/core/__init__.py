0ad658da046ebcd6f4dcc0ca94355edbe9d58dc0
Andrzej Górski <andrzejgorski@supermond.com>
2015-10-10 16:19:35 +0200

    Add core

---
+++ navgar/core/__init__.py
@@ -0,0 +1,26 @@
+# __init__.py
+
+from pyramid.config import Configurator
+from sqlalchemy import engine_from_config
+from sqlalchemy.orm import sessionmaker
+
+def db(request):
+    maker = request.registry.dbmaker
+    session = maker()
+
+    def cleanup(request):
+        if request.exception is not None:
+            session.rollback()
+        else:
+            session.commit()
+        session.close()
+    request.add_finished_callback(cleanup)
+
+    return session
+
+
+def main(global_config, **settings):
+    config = Configurator(settings=settings)
+    engine = engine_from_config(settings, prefix='sqlalchemy.')
+    config.registry.dbmaker = sessionmaker(bind=engine)
+    config.add_request_method(db, reify=True)
