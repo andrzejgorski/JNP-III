a9c7e5e01c67583fd836223d35809ebe23389b06
Andrzej Górski <andrzejgorski@supermond.com>
2015-11-10 17:11:24 +0100

    Add table users

--- navgar/navgar_core/database.py
+++ navgar/navgar_core/database.py
@@ -2,19 +2,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Sequence
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    MetaData,
+    Table,
+)
 
 
 Base = declarative_base()
-
-
-# url = os.environ.get('DATABASE_URL', 'url')
+metadata = MetaData()
 
 
 _engine = None
 
 
-def _get_engine(url):
+def _get_engine(url=None):
     global _engine
     if _engine is not None:
         return _engine
@@ -22,7 +26,7 @@ def _get_engine(url):
     return _engine
 
 
-def create_db_session(config):
+def create_db_session(config=None):
     engine = _get_engine(config['db-url'])
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
@@ -30,21 +34,28 @@ def create_db_session(config):
     return session
 
 
+users_tbl = Table(
+    'users',
+    Base.metadata,
+    Column('id', Integer, Sequence('user_id_seq'), primary_key=True),
+    Column('name', String(20), nullable=False),
+    Column('fullname', String(40)),
+    Column('password', String(40), nullable=False),
+)
+
+
 class User(Base):
     __tablename__ = 'users'
-
-    id = Column(Integer, Sequence('user_id_seq'), primary_key=True)
-    name = Column(String)
-    fullname = Column(String)
-    password = Column(String)
 
     def __repr__(self):
         return "<User(name='{}', fullname='{}', password='{}')>".format(
             self.name, self.fullname, self.password
         )
 
+# mapper(User, users_tbl,
 
 def recreate_db():
     engine = _get_engine()
-    for tbl in reversed(meta.sorted_tables):
+    for tbl in reversed(Base.metadata.sorted_tables):
         engine.execute(tbl.delete())
+    Base.metadata.create_all(engine)
--- navgar/navgar_tests/db_tests.py
+++ navgar/navgar_tests/db_tests.py
@@ -3,14 +3,18 @@ import navgar_core.database
 from navgar_core.database import (
     create_db_session,
     User,
+    recreate_db,
 )
 
 
 def test_connection():
     session = create_db_session(config)
+    recreate_db()
     ed_user = User(name='ed', fullname='Ed Jones', password='edspassword')
     session.add(ed_user)
     session.commit()
 
-    our_user = session.query(User).filter_by(name='ed').first()
-    assert our_user is ed_user
+    our_user = session.query(User).filter_by(name='ed').one()
+    assert our_user.name == 'ed'
+    assert our_user.fullname == 'Ed Jones'
+    assert our_user.password == 'edspassword'
